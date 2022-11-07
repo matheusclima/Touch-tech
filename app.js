bb77552c7dce2d7accdfecbc5a91ac109deee27f
Yanah Eisenbach <yanahsacha@gmail.com>
2022-11-07 20:15:18 -0300

    criar rota e controller do login

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const ProdutosRouter = require('./routers/ProdutosRouter')
+const LoginRouter = require('./routers/LoginRouter')
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-
-app.use('/produtos', ProdutosRouter)
-
 app.get('/', (req, res) => {
     res.render('home.ejs')
 });
 
+app.use('/produtos', ProdutosRouter)
 
-app.listen(3000, console.log('Servidor rodando na porta 3000'));
+app.use('/login', LoginRouter)
+
+
+
+
